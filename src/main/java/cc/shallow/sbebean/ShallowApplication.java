f7021e74e4156bc2d496083b8c7963128c4cc7d6
shenzhou-6 <yizmao.me@gmail.com>
2019-05-30 13:58:44 +0800

    rename

--- src/main/java/cc/shallow/sbebean/ShallowApplication.java
+++ src/main/java/cc/shallow/sbebean/ShallowApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiabetesApplication {
+public class ShallowApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiabetesApplication.class, args);
+		SpringApplication.run(ShallowApplication.class, args);
 	}
 
 }
